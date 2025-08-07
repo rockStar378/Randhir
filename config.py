bf203e01e17628414bf599e0d98389648e2f3af9
rockStar378 <shahzadluqman092@gmail.com>
2025-08-07 13:19:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,13 +16,13 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
-DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 1700))
+DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 900000))
 
 # Chat id of a group for logging bot's activities
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", -1002389305159))
-LOGGER_ID = LOG_GROUP_ID
-# Get this value from @MissRose_Bot on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", 7604553933))
+LOGGER_ID = int(getenv("LOGGER_ID", None))
+
+# Get this value from @FallenxBot on Telegram by /id
+OWNER_ID = int(getenv("OWNER_ID", None))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
@@ -30,26 +30,23 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # Get it from http://dashboard.heroku.com/account
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
-API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
-API_KEY = getenv("API_KEY", None) # youtube song api ke
-
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/rockStar378/Randhir",
+    "https://github.com/ankitsingh12134/Queen_ankit123",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/+eDGuiLMGvO0zNmFl")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/+eDGuiLMGvO0zNmFl")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+eDGuiLMGvO0zNmFl")
+
+API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') 
+API_KEY = getenv("API_KEY", '30DxNexGenBots667d40')
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
-AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", True))
-
-# make your bots privacy from telegra.ph and put your url here 
-PRIVACY_LINK = getenv("PRIVACY_LINK", "https://telegra.ph/Privacy-Policy-for-JaniMusic-08-14")
+AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
 
 
 # Get this credentials from https://developer.spotify.com/dashboard
@@ -63,11 +60,11 @@ PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", 25))
 
 # Telegram audio and video file size limit (in bytes)
 TG_AUDIO_FILESIZE_LIMIT = int(getenv("TG_AUDIO_FILESIZE_LIMIT", 104857600))
-TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 2145386496))
+TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 1073741824))
 # Checkout https://www.gbmb.org/mb-to-bytes for converting mb to bytes
 
 
-# Get your pyrogram v2 session from Replit
+# Get your pyrogram v2 session from @StringFatherBot on Telegram
 STRING1 = getenv("STRING_SESSION", None)
 STRING2 = getenv("STRING_SESSION2", None)
 STRING3 = getenv("STRING_SESSION3", None)
@@ -84,21 +81,21 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://files.catbox.moe/jrlhtb.jpg"
+    "START_IMG_URL", "https://files.catbox.moe/74y7ob.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://files.catbox.moe/jrlhtb.jpg"
+    "PING_IMG_URL", "https://files.catbox.moe/74y7ob.jpg"
 )
-PLAYLIST_IMG_URL = "https://files.catbox.moe/jrlhtb.jpg"
-STATS_IMG_URL = "https://files.catbox.moe/jrlhtb.jpg"
-TELEGRAM_AUDIO_URL = "https://files.catbox.moe/jrlhtb.jpg"
-TELEGRAM_VIDEO_URL = "https://files.catbox.moe/jrlhtb.jpg"
-STREAM_IMG_URL = "https://files.catbox.moe/jrlhtb.jpg"
-SOUNCLOUD_IMG_URL = "https://files.catbox.moe/jrlhtb.jpg"
-YOUTUBE_IMG_URL = "https://files.catbox.moe/jrlhtb.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://files.catbox.moe/jrlhtb.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://files.catbox.moe/jrlhtb.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://files.catbox.moe/jrlhtb.jpg"
+PLAYLIST_IMG_URL = "https://files.catbox.moe/yd51bx.jpg"
+STATS_IMG_URL = "https://files.catbox.moe/yd51bx.jpg"
+TELEGRAM_AUDIO_URL = "https://files.catbox.moe/yd51bx.jpg"
+TELEGRAM_VIDEO_URL = "https://files.catbox.moe/yd51bx.jpg"
+STREAM_IMG_URL = "https://files.catbox.moe/yd51bx.jpg"
+SOUNCLOUD_IMG_URL = "https://files.catbox.moe/yd51bx.jpg"
+YOUTUBE_IMG_URL = "https://files.catbox.moe/yd51bx.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://files.catbox.moe/yd51bx.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://files.catbox.moe/yd51bx.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://files.catbox.moe/yd51bx.jpg"
 
 
 def time_to_seconds(time):
@@ -115,14 +112,8 @@ if SUPPORT_CHANNEL:
             "[ERROR] - Your SUPPORT_CHANNEL url is wrong. Please ensure that it starts with https://"
         )
 
-if SUPPORT_GROUP:
-    if not re.match("(?:http|https)://", SUPPORT_GROUP):
+if SUPPORT_CHAT:
+    if not re.match("(?:http|https)://", SUPPORT_CHAT):
         raise SystemExit(
-            "[ERROR] - Your SUPPORT_GROUP url is wrong. Please ensure that it starts with https://"
-        )
-
-
-
-
-
-
+            "[ERROR] - Your SUPPORT_CHAT url is wrong. Please ensure that it starts with https://"
+)
